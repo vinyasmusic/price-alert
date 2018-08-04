476836cc9c5adedfece4ea3e6f4ae155e6b97908
Vinyas <vinyas@reckonsys.com>
2018-08-04 23:56:05 +0530

    html changes

--- stock_price_alert/alerts/urls.py
+++ stock_price_alert/alerts/urls.py
@@ -4,7 +4,7 @@ from .views import view_alerts, add_alerts
 
 app_name = "alerts"
 urlpatterns = [
-    path("view_alerts/", view=view_alerts, name="set_alerts"),
+    path("view_alerts/", view=view_alerts, name="view_alerts"),
     path("add_alerts/", view=add_alerts, name="add_alerts"),
 
 ]
