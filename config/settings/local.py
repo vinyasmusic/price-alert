b54d1da2942f56648976eb01e3bc5e60213744e0
Vinyas <vinyas@reckonsys.com>
2018-08-04 23:15:25 +0530

    revert back django debug for testing

--- config/settings/local.py
+++ config/settings/local.py
@@ -4,7 +4,7 @@ from .base import env
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = env.bool('DJANGO_DEBUG', False)
+DEBUG = env.bool('DJANGO_DEBUG', True)
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='2xkXDoFEk3nPQi0HRpJ7gQXoeKXQsWiReXKceznzoS9h0ZM6iFvq4pZX1Mxetmb4')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
